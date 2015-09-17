7cbc5e5b9b0448389ad8a16d3be56b5c4abf7e48
palat029 <TatyanaPalacios@tatyanas-mbp.wireless.newschool.edu>
2015-09-17 14:40:21 -0400

    FlyingTacoReady

--- sketch.js
+++ sketch.js
@@ -2,7 +2,8 @@ function setup() {
 	// create a place to draw
 	createCanvas(640, 360);
 	noStroke();
-  noLoop();
+	noLoop();
+
 }
 
 function draw() {
@@ -12,7 +13,7 @@ function draw() {
 	// set a fill color
 	fill(255, 255, 255);
 
-	// draw the ellipse
+	// draw the ellipse face
 fill(211,162,116);
 	ellipse(25, 180, 500, 500);
 fill(35,31,32);
@@ -29,20 +30,32 @@ fill(35,31,32);
 	ellipse(20, 30, diameter, diameter);
 	ellipse(10, 16, diameter, diameter);
 
+	// draw ear
+  fill(184,150,116);
+	ellipse(20, 180, 20,30);
+
+
 	//draw mouth
 		var x = random(-40, 0);
 	fill(255,255,255);
 			ellipse(200 , 280, 100, 120 + x);
 
  //draw flying taco
-		var halftaco = random(100, 100);
-		var y = random(70);
-	fill(232, 182, 33);
-				ellipse(500 - y, 280,halftaco, halftaco);
-	fill(255, 253, 170);
-	 			rect(440 - y, 200, 120, 80);
+		var y = random(100);
+		var Lettuce = random(20,20);
 	fill(3, 208, 0);
-							 		ellipse(500 - y, 280, 20, 20);
+		 ellipse(500 - y, 280, Lettuce, Lettuce);
+		 ellipse(480 - y, 280, Lettuce, Lettuce);
+		 ellipse(520 - y, 280, Lettuce, Lettuce);
+	fill(232, 182, 33);
+				arc(500 - y, 280, 80, 80, 0, PI);
+	fill(54,33,0);
+			var tortillatext;
+	 		tortillatext= random(1, 3);
+			ellipse(500 - y,290, tortillatext, tortillatext);
+			ellipse(480 - y,300, tortillatext, tortillatext);
+			ellipse(510 - y,310, tortillatext, tortillatext);
+			ellipse(530 - y,295, tortillatext, tortillatext);
 
 
 
